80b7802564a79a67c01dc818a5719dc511dd0a8a
choheejin <hjcho9510@gmail.com>
2021-11-09 22:41:42 +0900

    :lipstick: 첫 화면, 로그인 및 회원가입 화면 추가 (#15)

---
+++ src/main/java/application/gallery/App.java
@@ -0,0 +1,30 @@
+package application.gallery;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.text.Font;
+
+import java.io.IOException;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        System.setProperty("prism.lcdtext", "false");
+        Font.loadFont(getClass().getResourceAsStream("MapoFlowerIsland.ttf"), 10);
+        Font.loadFont(getClass().getResourceAsStream("MapoGoldenPier.ttf"), 10);
+        Font.loadFont(getClass().getResourceAsStream("Roboto-Medium.ttf"), 10);
+        Font.loadFont(getClass().getResourceAsStream("MapoPeacefull.ttf"), 10);
+
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("MainScene.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1100, 700);
+        stage.setTitle("한눈의 갤러리");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
