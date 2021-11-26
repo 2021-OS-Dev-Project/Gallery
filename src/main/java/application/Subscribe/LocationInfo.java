dc5f3cc02b96fea0a3fb3dbd0934afcda61c256e
OhJiwoo <74577768+Ohjiwoo-lab@users.noreply.github.com>
2021-11-26 11:51:49 +0900

    ✨ 구독 및 알림 기능 구현 (#21) (#25)
    * ✨ 전시회 추가 여부 감지하는 기능 구현 (#21)
    
    * ✨ 3개의 미술관에 대한 구독 기능 구현 (#21)
    
    * ✨ 전시회 추가시 팝업이 뜨는 기능 구현 (#21)
    
    * ✨ 전체 미술관에 대한 구독 기능 완성 (#21)

---
+++ src/main/java/application/Subscribe/LocationInfo.java
@@ -0,0 +1,22 @@
+package application.Subscribe;
+
+public class LocationInfo {
+
+    private int LocNum;
+    private String City;
+
+    LocationInfo(int locNum,String city){
+        this.LocNum=locNum;
+        this.City=city;
+    }
+    public int getLocNum() {
+        return LocNum;
+    }
+    public String getCity() {
+        return City;
+    }
+    public void setCity(String city) {
+        City = city;
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
-module application.gallery {
+module application {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires TrayTester;
+    requires commons.collections4;
 
     opens application.gallery to javafx.fxml;
     exports application.gallery;
+
+    opens application.Subscribe to javafx.fxml;
+    exports application.Subscribe;
 }
